18520ccb76225f6de93d7f472b3ba9c5da1d3211
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-06-17 13:48:37 +0100

    basic python operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 6
+number2 = 10
+
+
+sum = number1 + number2
+minus = number1 - number2
+multiplication = number1 * number2
+
+print("Addition of ",  number1,  "and ",  number2,  "is ", sum)
+print("Subtraction of ",  number1,  "from ",  number2,  "is ", minus)
+print("Multiplication of ",  number1,  "and ",  number2,  "is ", multiplication)
