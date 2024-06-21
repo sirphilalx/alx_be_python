0162e04cbf90ef6be31a4d4ca5891ec07ca575d4
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-06-21 12:56:36 +0100

    hot fix

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,6 +5,6 @@ monthly_savings = monthly_income - monthly_expense
 
 print(f'your monthly savings are ${monthly_savings}')
 
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+projected_savings = round(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 print(f'Projected savings after one year, with interest, is: ${projected_savings}')
