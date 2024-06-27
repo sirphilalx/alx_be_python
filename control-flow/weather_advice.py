87348b03db0b6a2c43ef43d7074257af1938fd26
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-06-27 12:18:59 +0100

    hot fix

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,12 +2,12 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 
 if weather == 'sunny':
-    message = 'Wear a t-shirt and sunglasses.'
-elif weather == 'rainy':
-    message = 'Don\'t forget your umbrella and a raincoat.'
-elif weather == 'cold':
-    message = 'Make sure to wear a warm coat and a scarf.'
+    message = "Wear a t-shirt and sunglasses."
+elif weather == "rainy":
+    message = "Don't forget your umbrella and a raincoat."
+elif weather == "cold":
+    message = "Make sure to wear a warm coat and a scarf."
 else:
-    message = 'Sorry, I don\'t have recommendations for this weather.' 
+    message = "Sorry, I don't have recommendations for this weather." 
 
 print(message)
