ea2607d882c0a2dda25729d5a711f8e05ec8ee33
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-07-02 10:29:47 +0100

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@
 def perform_operation(num1, num2, operation):
     """a function that performs basic arithmetic operations"""
     if operation == 'divide':
-        if (num1 == 0 or num2 == 0):
+        if num2 == 0:
             return 'division by zero is not supported'
         else:
             return num1 / num2
