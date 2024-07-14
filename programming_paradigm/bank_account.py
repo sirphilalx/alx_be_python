254d4e662c1f1de23b8c27fb743abde7bfaabc2b
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-07-14 16:01:21 +0100

    bank app

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        return self.account_balance + amount
+    
+    def withdraw(self, amount):
+        if (amount < self.account_balance):
+            return True
+        else:
+            return False
+    def display_balance(self):
+        return self.account_balance
